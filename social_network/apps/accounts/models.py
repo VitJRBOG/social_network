be666fabe5e581304de258d92a8199f7856ee0dd
VitJRBOG <vitjrbog@gmail.com>
2022-05-16 16:44:48 +0500

    Described the models of 'accounts' app

--- social_network/apps/accounts/models.py
+++ social_network/apps/accounts/models.py
@@ -1,1 +1,30 @@
 from django.db import models
+
+class Profile(models.Model):
+    id = models.AutoField('Идентификатор', primary_key=True)
+    name = models.TextField('Полное имя', max_length=256)
+
+    def __init__(self, name: str):
+        super().__init__()
+
+        self.name = name
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+
+class Following(models.Model):
+    id = models.AutoField('Идентификатор', primary_key=True)
+    profile_id = models.IntegerField('Идентификатор профиля')
+    blog_id = models.IntegerField('Идентификатор блога')
+
+    def __init__(self, profile_id: int, blog_id: int):
+        super().__init__()
+
+        self.profile_id = profile_id
+        self.blog_id = blog_id
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
