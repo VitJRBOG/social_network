fbbca50d40d95fc1210f6ac08afd9ce766af433c
VitJRBOG <vitjrbog@gmail.com>
2022-05-16 16:49:39 +0500

    Described the models of 'sources' app

--- social_network/apps/sources/models.py
+++ social_network/apps/sources/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    id = models.AutoField('Идентификатор', primary_key=True)
+    profile_id = models.IntegerField('Идентификатор профиля')
+
+    def __init__(self, profile_id: int):
+        super().__init__()
+
+        self.profile_id = profile_id
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
+
+
+class BlogPost(models.Model):
+    id = models.AutoField('Идентификатор', primary_key=True)
+    blog_id = models.IntegerField('Идентификатор блога')
+
+    def __init__(self, blog_id: int):
+        super().__init__()
+
+        self.blog_id = blog_id
+
+    class Meta:
+        verbose_name = 'Пост блога'
+        verbose_name_plural = 'Посты блога'
