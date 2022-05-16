575d426a449b994c91f6553d8154ccd39d8bdd0e
VitJRBOG <vitjrbog@gmail.com>
2022-05-16 16:55:45 +0500

    Described the models of 'feeds' app

--- social_network/apps/feeds/models.py
+++ social_network/apps/feeds/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class FeedPost(models.Model):
+    id = models.AutoField('Идентификатор', primary_key=True)
+    profile_id = models.IntegerField('Идентификатор профиля')
+    blogpost_id = models.IntegerField('Идентификатор поста блога')
+    is_read = models.BooleanField('Прочитано')
+
+    def __init__(self, profile_id: int, blogpost_id: int, is_read: bool):
+        super().__init__()
+
+        self.profile_id = profile_id
+        self.blogpost_id = blogpost_id
+        self.is_read = is_read
+
+    class Meta:
+        verbose_name = 'Пост ленты'
+        verbose_name_plural = 'Посты ленты'
