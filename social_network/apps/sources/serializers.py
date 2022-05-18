e53bfc1fe3ca9742585ae2712c0b1d539756ecfa
VitJRBOG <vitjrbog@gmail.com>
2022-05-18 18:16:03 +0500

    Models BlogSerializer and BlogPostSerializer has been changed

--- social_network/apps/sources/serializers.py
+++ social_network/apps/sources/serializers.py
@@ -5,7 +5,7 @@ from .models import Blog, BlogPost
 class BlogSerializer(serializers.Serializer):
     id = serializers.PrimaryKeyRelatedField(read_only=True)
     name = serializers.CharField(max_length=256)
-    profile = serializers.IntegerField(source='profile.id')
+    profile_id = serializers.IntegerField()
 
     def create(self, validated_data):
         return Blog.objects.create(**validated_data)
@@ -16,7 +16,7 @@ class BlogPostSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=50)
     text = serializers.CharField(max_length=140)
     date = serializers.IntegerField()
-    blog = serializers.IntegerField(source='blog.id')
+    blog_id = serializers.IntegerField()
 
     def create(self, validated_data):
         return BlogPost.objects.create(**validated_data)
