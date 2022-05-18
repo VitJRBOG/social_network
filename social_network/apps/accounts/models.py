76a4363acb06ff6b39d9849393203e85c3dceedf
VitJRBOG <vitjrbog@gmail.com>
2022-05-18 10:06:52 +0500

    Fields 'login' and 'password_hash' was added to Profile model

--- social_network/apps/accounts/models.py
+++ social_network/apps/accounts/models.py
@@ -3,11 +3,15 @@ from django.db import models
 class Profile(models.Model):
     id = models.AutoField('Идентификатор', primary_key=True)
     name = models.CharField('Полное имя', max_length=256)
+    login = models.CharField('Имя пользователя', max_length=25)
+    password_hash = models.CharField('Хэш пароля', max_length=256)
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, login: str, password_hash: str):
         super().__init__()
 
         self.name = name
+        self.login = login
+        self.password_hash = password_hash
 
     class Meta:
         verbose_name = 'Профиль'
