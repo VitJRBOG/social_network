d6522287c1dbb79185d1727f9dac5dbbe2c097c5
VitJRBOG <vitjrbog@gmail.com>
2022-05-19 16:01:06 +0500

    Models and serializers of Feeds app has been deleted

--- social_network/apps/feeds/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import FeedPost
-
-
-admin.site.register(FeedPost)
--- social_network/apps/feeds/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-
-class FeedPostSerializer(serializers.Serializer):
-    id = serializers.PrimaryKeyRelatedField(read_only=True)
-    profile = serializers.IntegerField(source='profile.id')
-    blogpost = serializers.IntegerField(source='blogpost.id')
-    is_read = serializers.BooleanField()
